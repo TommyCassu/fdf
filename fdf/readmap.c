e3e04e4472a435325fa5db59537969aa85322868
Tommy <90561323+TommyCassu@users.noreply.github.com>
2025-03-24 16:34:33 +0100

    Update readmap.c

--- fdf/readmap.c
+++ fdf/readmap.c
@@ -6,7 +6,7 @@
 /*   By: tcassu <tcassu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 02:09:56 by tcassu            #+#    #+#             */
-/*   Updated: 2025/03/24 04:14:34 by tcassu           ###   ########.fr       */
+/*   Updated: 2025/03/24 16:13:28 by tcassu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,13 @@ void	ft_freetab(char **result)
 	}
 	free(result);
 }
+
 int	ft_fill_line(char **data, t_map *map)
 {
 	int	i;
 	int	height;
 	char	**tmp;
+	
 	i = 0;
 	tmp = data;
 	while (*tmp)
@@ -51,6 +53,7 @@ int	ft_fill_line(char **data, t_map *map)
 	map->heightmap[map->size.y][i] = PX_END_OF_LINE;
 	return (i);
 }
+
 t_map	*readfile(t_map *map, int fd)
 {
 	char	**tab;
@@ -69,12 +72,73 @@ t_map	*readfile(t_map *map, int fd)
 	}
 	return (map);
 }
+int	ft_mappingHeight(char *filename)
+{
+	int	i;
+	int	fd;
+	char	*line;
+	
+	fd = open(filename, O_RDONLY);
+	i = 0;
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		i++;
+		free(line);
+	}
+	return (i);
+}
 void	ft_readmap(t_map *map, char *filename)
 {
 	int	fd;
 	
-	map->size.y = 0;
+	map->size.y = ft_mappingHeight(filename);
+	map->heightmap = malloc(sizeof(int *) * map->size.y);
 	fd = open(filename, O_RDONLY);
+	map->size.y = 0;
 	readfile(map, fd);
 }
+/* 
+#include "fdf.h"
+#include <stdio.h>
 
+int main(int argc, char **argv)
+{
+    t_map map;
+    int i, j;
+
+    if (argc != 2)
+    {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    // Initialisation de la map
+    map.size.x = 0;
+    map.size.y = 0;
+    map.heightmap = NULL;
+
+    // Lecture de la map
+    ft_readmap(&map, argv[1]);
+
+    // Affichage des résultats
+    printf("Map size: %d x %d\n", map.size.x, map.size.y);
+    printf("Heightmap:\n");
+    for (i = 0; i < map.size.y; i++)
+    {
+        for (j = 0; map.heightmap[i][j] != PX_END_OF_LINE; j++)
+        {
+            printf("%3d ", map.heightmap[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Libération de la mémoire
+    for (i = 0; i < map.size.y; i++)
+    {
+        free(map.heightmap[i]);
+    }
+    free(map.heightmap);
+
+    return 0;
+}
+*/
